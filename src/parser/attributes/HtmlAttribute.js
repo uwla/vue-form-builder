fa3a499468839d2dae804e8e128e15f24667aed3
uwla <uwlalinux@gmail.com>
2023-07-29 13:11:59 -0300

    cast string attributes which represents boolean values

--- src/parser/attributes/HtmlAttribute.js
+++ src/parser/attributes/HtmlAttribute.js
@@ -6,6 +6,11 @@ export default class HtmlAttribute {
     static stringAttributeToObject(attribute) {
         const key = attribute.split('=')[0]
         const value = attribute.split('=')[1]
+
+        // cast string values to boolean
+        if (value === 'true') value = true
+        if (value === 'false') value = false
+
         return { htmlAttributes: { [key]: value } }
     }
 }
