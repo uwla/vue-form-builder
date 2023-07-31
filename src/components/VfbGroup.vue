bc031df4ef9ee8091bec434ee9def4b953f7e327
uwla <uwlalinux@gmail.com>
2023-07-30 21:50:35 -0300

    add group component

---
+++ src/components/VfbGroup.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="vfb-group">
+        <label>{{ label }}</label>
+        <slot></slot>
+    </div>
+</template>
+<script src="./VfbGroup.js"></script>
+<style src="./VfbGroup.css"></style>
