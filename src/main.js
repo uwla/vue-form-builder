60d9aa7858ad2ceb574d2ecae250fdc61aa43b39
uwla <uwlalinux@gmail.com>
2023-07-31 15:36:51 -0300

    register new components in install function

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
 import VueFormBuilder from './components/VueFormBuilder.vue'
+import VfbCheckboxes from './components/VfbCheckboxes.vue'
+import VfbCheckbox from './components/VfbCheckbox.vue'
 import VfbButtons from './components/VfbButtons.vue'
 import VfbSelect from './components/VfbSelect.vue'
 import VfbGroup from './components/VfbGroup.vue'
+import VfbInput from './components/VfbInput.vue'
+import VfbFile from './components/VfbFile.vue'
 
 function install(Vue) {
+    Vue.component('vfb-checkboxes', VfbCheckboxes)
+    Vue.component('vfb-checkbox', VfbCheckbox)
     Vue.component('vfb-buttons', VfbButtons)
     Vue.component('vfb-select', VfbSelect)
     Vue.component('vfb-group', VfbGroup)
+    Vue.component('vfb-input', VfbInput)
+    Vue.component('vfb-file', VfbFile)
     Vue.component('vfb', VueFormBuilder)
     Vue.component('vue-form-builder', VueFormBuilder)
 }
 
-export { VfbButtons, VfbGroup, VueFormBuilder, install as default }
+export {
+    VfbFile,
+    VfbGroup,
+    VfbInput,
+    VfbSelect,
+    VfbButtons,
+    VfbCheckbox,
+    VfbCheckboxes,
+    VueFormBuilder,
+    install as default,
+}
