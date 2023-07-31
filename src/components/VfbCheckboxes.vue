dcf49a6597084b116c74828634b3dd23b5a52675
uwla <uwlalinux@gmail.com>
2023-07-31 15:34:47 -0300

    add checkboxes component

---
+++ src/components/VfbCheckboxes.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="vfb-checkboxes">
+        <li v-for="(option,i) in options" :key="i">
+            <input type="checkbox" :value="option.value">
+            <span>{{ option.text }}</span>
+        </li>
+    </ul>
+</template>
+<script src="./VfbCheckboxes.js"></script>
+<style src="./VfbCheckboxes.css"></style>
