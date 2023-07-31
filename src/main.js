d48fcae9a9eb08967e92f9469605c35792c86834
uwla <uwlalinux@gmail.com>
2023-07-30 21:49:31 -0300

    export an install function to register the components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import VueFormBuilder from './components/VueFormBuilder.vue'
 import VfbButtons from './components/VfbButtons.vue'
+import VfbSelect from './components/VfbSelect.vue'
+import VfbGroup from './components/VfbGroup.vue'
 
-export {
-    VfbButtons,
-    VueFormBuilder,
-    VueFormBuilder as default,
+function install(Vue) {
+    Vue.component('vfb-buttons', VfbButtons)
+    Vue.component('vfb-select', VfbSelect)
+    Vue.component('vfb-group', VfbGroup)
+    Vue.component('vfb', VueFormBuilder)
+    Vue.component('vue-form-builder', VueFormBuilder)
 }
+
+export { VfbButtons, VfbGroup, VueFormBuilder, install as default }
