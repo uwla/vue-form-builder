94d008cadc261411bc96fa2601d5769e250b3086
uwla <uwlalinux@gmail.com>
2023-08-07 15:08:58 -0300

    add integration tests for form submission and reset

--- tests/components.test.js
+++ tests/components.test.js
@@ -25,6 +25,19 @@ const fields = [
     'component:vfb-buttons|label:none|submitText=SUBMIT|resetText=RESET'
 ]
 
+// the model, which will be later used.
+const model = {
+    name: 'john',
+    email: 'john@email.test',
+    gender: 'male',
+    phone: '+1 999 9999-9999',
+    fruits: ['banana', 'avocado'],
+    website: 'http://example.test/',
+    country: 'Mexico',
+    agree: true,
+    bio: 'Hello, this is John Doe from Mexico. I like bananas.',
+}
+
 const wrapper = mount(VueFormBuilder, {
     propsData: {
         fields,
@@ -223,34 +236,21 @@ test('it hides messages', async () => {
     expect(wrapper.findAll(`.vfb-feedback-valid.visible`)).toHaveLength(0)
 })
 
-test('it sync with the model', async () => {
-    const model = {
-        name: 'john',
-        email: 'john@email.test',
-        gender: 'male',
-        phone: '+1 999 9999-9999',
-        fruits: ['banana', 'avocado'],
-        website: 'http://example.test/',
-        country: 'Mexico',
-        agree: true,
-        bio: 'Hello, this is John Doe from Mexico. I like bananas.',
-    }
-
+test('it syncs with the model', async () => {
     await wrapper.setProps({ model })
 
     for (let key in model)
     {
         let value = model[key]
 
-        // select
+        // radio options
         if (key === 'country')
         {
-            const select = wrapper.find(`[name=${key}]`)
-            const options = select.findAll('option')
-            for (let i = 0; i < options.length; i+=1)
+            const radios = wrapper.findAll(`[name=${key}]`)
+            for (let i = 0; i < radios.length; i+=1)
             {
-                let option = options.at(i).element
-                expect(option.selected).toBe(option.value === value)
+                let radio = radios.at(i).element
+                expect(radio.checked).toBe(radio.value === value)
             }
             continue
         }
@@ -284,7 +284,89 @@ test('it sync with the model', async () => {
         }
     }
 })
-// reset form
-// messages
+
+test('it submits the form correctly', async () => {
+    const values  = {
+        name: 'Mary Lindsey',
+        email: 'mary@email.test',
+        phone: '+100 12345 6789',
+        website: 'http://mary.me',
+        birthday: '1980-01-01',
+        amount: '10',
+        bio: 'hello world!',
+        gender:  'female',
+        fruits: ['apple', 'orange'],
+        country: 'Mexico',
+        agree: false,        
+    } 
+
+    for (let key in values)
+    {
+        let val = values[key]
+
+        if (key === 'fruits')
+        {
+            const checkboxes = wrapper.findAll(`[name=${key}]`)
+            for (let i = 0; i < checkboxes.length; i += 1)
+            {
+                let checkbox = checkboxes.at(i)
+                checkbox.setChecked(val.includes(checkbox.element.value))
+            }
+            continue
+        }
+
+        if (key === 'country')
+        {
+            const radios = wrapper.findAll(`[name=${key}]`)
+            for (let i = 0; i < radios.length; i += 1)
+            {
+                let radio = radios.at(i)
+                if (val === radio.element.value)
+                    radio.setChecked()
+            }
+            continue
+        }
+
+        if (typeof val === 'string')
+        {
+            const input = wrapper.find(`[name=${key}]`)
+            input.setValue(val)
+        }
+
+        if (val === true || val === false)
+        {
+            const checkbox = wrapper.find(`[name=${key}]`)
+            checkbox.setChecked(val)
+        }
+    }
+
+    // trigger a submit event
+    await wrapper.trigger('submit')
+
+    // get the event object
+    const event = wrapper.emitted('submit')[0]
+
+    // assert event has been emitted
+    expect(event).toBeTruthy()
+
+    // assert payload
+    const payload = event[0]
+    expect(payload).toMatchObject(values)
+})
+
+test('it resets the form', async () => {
+    await wrapper.trigger('reset')
+    await wrapper.trigger('submit')
+
+    // get the event object (which is the second submit event)
+    const event = wrapper.emitted('submit')[1]
+
+    // assert event has been emitted
+    expect(event).toBeTruthy()
+
+    // assert payload matches model
+    const payload = event[0]
+    expect(payload).toMatchObject(model)
+})
+
 // validation
-// errors
