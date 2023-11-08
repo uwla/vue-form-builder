2aba912af9cad99a6d63d2b101ac4fa401291345
uwla <uwlalinux@gmail.com>
2023-11-08 09:13:25 -0300

    move providers to its own folder

---
+++ src/provider/index.ts
@@ -0,0 +1,35 @@
+import { VfbProvider, BootstrapVueProvider } from "./providers"
+
+const providers : Providers = {
+    'vfb': VfbProvider,
+    'bootstrap-vue': BootstrapVueProvider,
+}
+
+function addProvider(name: ProviderName, provider: ComponentProvider) {
+    if (! Object(providers).hasOwnProperty(name))
+        providers[name] = provider
+}
+
+function delProvider(name: ProviderName) {
+    delete providers[name]
+}
+
+function getProvider(name: ProviderName) {
+    return providers[name]
+}
+
+function hasProvider(name: ProviderName) {
+    return Object(providers).hasOwnProperty(name)
+}
+
+function setProvider(name: ProviderName, provider: ComponentProvider) {
+    providers[name] = provider
+}
+
+export const ProviderService = {
+    addProvider,
+    delProvider,
+    getProvider,
+    hasProvider,
+    setProvider,
+}
