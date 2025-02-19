957ddfcf32898cc2e34ae5d56e40b8e19054d266
uwla <uwlalinux@gmail.com>
2025-02-19 09:15:48 -0300

    chore: set up eslint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config(
+    {
+        ignores: ['*.d.ts', '**/coverage', '**/dist', 'assets/'],
+        extends: [
+            eslint.configs.recommended,
+            ...typescriptEslint.configs.recommended,
+            ...eslintPluginVue.configs['flat/recommended'],
+        ],
+        files: ['**/*.{ts,vue}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: globals.browser,
+            parserOptions: {
+                parser: typescriptEslint.parser,
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off", // Allow the use of `any`
+            "vue/multi-word-component-names": "off", // Disable multi-word component names rule
+            "vue/require-default-prop": "off", // Disable requiring default props
+            "vue/no-v-html": "off" // Allow v-html usage (if needed)
+        },
+    },
+    eslintConfigPrettier
+);
